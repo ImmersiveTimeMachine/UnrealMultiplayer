61f42c82e392af05eba808be1321edf7d68cf321
ITM <artur@immersivetimemachine.com>
2020-10-18 21:54:26 -0400

    NumberOfPlayers >= 3

--- Source/PuzzlePlatforms/LobbyGameMode.cpp
+++ Source/PuzzlePlatforms/LobbyGameMode.cpp
@@ -8,7 +8,7 @@ void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
 	Super::PostLogin(NewPlayer);
 
 	++NumberOfPlayers;
-	if (NumberOfPlayers >= 2)
+	if (NumberOfPlayers >= 3)
 	{
 		UWorld* World = GetWorld();
 		if (!ensure(World != nullptr)) return;
